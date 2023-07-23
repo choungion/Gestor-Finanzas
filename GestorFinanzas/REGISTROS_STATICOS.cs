f1881e204d2b263cf281ed29e3edd4dfebc7c990
choungion <leefuentespablo.1@hotmail.es>
2023-07-22 22:44:42 -0600

    Inicio sesion

---
+++ GestorFinanzas/REGISTROS_STATICOS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorFinanzas
+{
+    public static class REGISTROS_STATICOS
+    {
+        public static DATOS_PERSONA[] ARREGLO_DE_USUARIOS = new DATOS_PERSONA[20];
+
+    }
+}
---
+++ GestorFinanzas/WindowInicioSesion.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GestorFinanzas
+{
+    public partial class WindowInicioSesion : Window
+    {
+        public WindowInicioSesion()
+        {
+            InitializeComponent();
+            DATOS_PERSONA ADMINISTRADOR = new DATOS_PERSONA("ADMIN", "");
+            REGISTROS_STATICOS.ARREGLO_DE_USUARIOS[0] = ADMINISTRADOR;
+        }
+
+
+        private void INICIAR_sesion(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < REGISTROS_STATICOS.ARREGLO_DE_USUARIOS.Length; i++)
+            {
+                if (REGISTROS_STATICOS.ARREGLO_DE_USUARIOS[i] != null)
+                {
+                    if (this.txt_USERNAME.Text == REGISTROS_STATICOS.ARREGLO_DE_USUARIOS[i].User1 && this.txt_PASSWORD.Password == REGISTROS_STATICOS.ARREGLO_DE_USUARIOS[i].Password)
+                    {
+                        MessageBox.Show("the password has been passed ");
+                        MainWindow INICIO = new MainWindow();
+                        INICIO.Show();
+                        Hide();
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        private void REGISTRO_usuario(object sender, RoutedEventArgs e)
+        {
+
+            WindowRegistroUsuario ventana_REGISTROS = new WindowRegistroUsuario();
+            ventana_REGISTROS.Show();
+        }
+    }
+}
+
