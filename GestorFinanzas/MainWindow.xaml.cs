174a0ced8f4852f98977b9b1aa9dda4ccab476bb
choungion <leefuentespablo.1@hotmail.es>
2023-07-22 21:03:13 -0600

    Initial Commit

---
+++ GestorFinanzas/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GestorFinanzas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonPeriodo_Click(object sender, RoutedEventArgs e)
+        {
+            Calendario.Visibility = Visibility.Visible;
+        }
+        private void Calendario_DisplayModeChanged(object sender, CalendarModeChangedEventArgs e)
+        {
+            Calendario.DisplayMode = CalendarMode.Year;
+            Calendario.Visibility = Visibility.Hidden;
+        }
+        private void Calendario_DisplayDateChanged(object sender, CalendarDateChangedEventArgs e)
+        {
+            DateTime? fecha = e.AddedDate;
+            if (fecha.HasValue)
+            {
+                DateTime selectedMonth = fecha.Value;
+                LabelPeriodo.Content = $"{selectedMonth: MMMM yyyy}";
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+    }
+}
